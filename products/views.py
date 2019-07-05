d84e819fd330fee2b22b12d436bda679f3ec4c85
Geoff Davis <geoffdavis92@gmail.com>
2019-07-05 15:56:36 -0500

    update views config

--- products/views.py
+++ products/views.py
@@ -3,14 +3,30 @@ from .models import Product
 
 # Create your views here.
 def index(request):
-  return render(request, 'products/index.liquid')
+  active_products = Product.objects.filter(is_finished=False, is_expired=False).order_by('date_expires','name')
+  finished_products = Product.objects.filter(is_finished=True)
+  expired_products = Product.objects.filter(is_expired=True)
+  context = {
+    "active_products": active_products,
+    "finished_products": finished_products,
+    "expired_products": expired_products
+  }
+  return render(request, 'products/index.html', context)
+
+def product_detail(request, product_pk):
+  product = Product.objects.get(pk=product_pk)
+  return render(request,'products/detail.html', {
+    "header_show_back": True,
+    "product":product
+  })
 
 def category_index(request):
   category_list = []
+  print(Product.product_category_choices)
   for category_group_key, category_group_tuple in Product.product_category_choices:
-      for category_key, category_value in category_group_tuple:
-        category_list.append((category_key,category_value,category_value.replace(' ','-')))
-  return render(request, 'products/category_index.liquid', {
+      for category_value in category_group_tuple:
+        category_list.append((category_value,category_value.replace(' ','-')))
+  return render(request, 'products/category_index.html', {
     "categories": category_list
   })
 
@@ -28,7 +44,7 @@ def category_list(request, category_url):
     "category_url_replaced": category_url.replace('-',' '),
     "products_in_category": products_in_category
   })
-  return render(request, 'products/category_list.liquid', { 
+  return render(request, 'products/category_list.html', { 
     "category_key": category_key,
     "category_value": category_value,
     "products_in_category": products_in_category
