43af130b669e74d515a1d87ced5b1b47e7ce99f4
Geoff Davis <geoffdavis92@gmail.com>
2019-07-05 15:53:48 -0500

    add CategoryAdmin class, display is_expired for Product admin

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Product
+from .models import Category, Product
 # Register your models here.
 
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ("__str__", "group")
+  search_fields = ("name", "group")
+
 class ProductAdmin(admin.ModelAdmin):
-  list_display = (Product.__str__, "date_purchased", "date_finished", "is_finished")
+  list_display = (Product.__str__, "date_purchased", "date_finished", "is_finished", "is_expired")
   search_fields = ("name",)
   # list_filter = ["category"]
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
