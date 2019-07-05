b3ba795a417bc99b49e2a2fab1759312e253df18
Geoff Davis <geoffdavis92@gmail.com>
2019-07-05 15:54:57 -0500

    add product detail URL config

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name="products_index"),
+  path('<int:product_pk>', views.product_detail, name="product_detail"),
   path('categories', views.category_index, name="category_list"),
   path('category/<str:category_url>/', views.category_list, name="products_in_category")
 ] 
